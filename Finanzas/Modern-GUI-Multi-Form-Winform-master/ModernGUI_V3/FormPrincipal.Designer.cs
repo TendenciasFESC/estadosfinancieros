847d695fcc32c13123c79b7ec4b13b5f2db54a01
Efren <efren.raymo@gmail.com>
2019-11-18 14:24:29 -0600

    Correcciones

--- Finanzas/Modern-GUI-Multi-Form-Winform-master/ModernGUI_V3/FormPrincipal.Designer.cs
+++ Finanzas/Modern-GUI-Multi-Form-Winform-master/ModernGUI_V3/FormPrincipal.Designer.cs
@@ -158,7 +158,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(188, 40);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Estado de resultados";
             this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
