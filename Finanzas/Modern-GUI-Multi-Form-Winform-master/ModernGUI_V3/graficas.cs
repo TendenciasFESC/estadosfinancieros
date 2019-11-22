84c27ce734676d3b4ec54ac47bf07cdbb8877815
EFREN RAYMUNDO MORALES <raymo99@comunidad.unam.mx>
2019-11-21 23:22:10 -0600

    borrador graficas

---
+++ Finanzas/Modern-GUI-Multi-Form-Winform-master/ModernGUI_V3/graficas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ModernGUI_V3
+{
+    public partial class graficas : Form
+    {
+        public graficas()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+
+        private void graficas_Load(object sender, EventArgs e)
+        {
+            string[] series = { "Total activos", "Total pasivos", "Total capital" };
+            int[] puntos = { 23, 10, 70 };
+
+            chart1.Palette = ChartColorPalette.Pastel;
+            chart1.Titles.Add("2017");
+
+            for (int i = 0; i < series.Length; i++)
+            {
+                Series serie = chart1.Series.Add(series[i]);
+
+                serie.Label = puntos[i].ToString();
+
+                serie.Points.Add(puntos[i]);
+            }
+
+        }
+    }
+}
